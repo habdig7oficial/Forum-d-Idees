1f452e40d28746c747beb7f6d0054aa0f9ba02ec
habdig7oficial <65318468+habdig7oficial@users.noreply.github.com>
2021-05-15 20:55:01 -0300

    Add files via upload

---
+++ Forum d' Idées/js/switd.js
@@ -0,0 +1,27 @@
+function azul(){
+
+
+  
+window.open("stable.html")
+
+
+
+
+
+}
+
+
+function apple(){
+
+
+    alert("versão para macOS não disponível")
+
+}
+
+
+function windows(){
+
+alert("versão windows não disponível")
+
+
+}
